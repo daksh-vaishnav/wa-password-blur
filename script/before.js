c3948767e261334be5e5b09b33e46b0464cb49a1
daksh <vaishnav.dhaval.97@gmail.com>
2023-08-24 11:02:51 +0530

    chatscreen blue (WIP)

--- script/before.js
+++ script/before.js
@@ -14,7 +14,9 @@ chrome.storage.sync.get("password", ({ password }) => {
 				const divElements = document.querySelectorAll("._1AHcd"); // Replace with your class name
 				if (divElements.length > 0) {
 					clearInterval(interval); // Clear the interval when the class is found
-					modifyCSSContactList(); // Call the function to modify the CSS
+					addCSS();
+					blurContactCSS(); // Call the function to modify the CSS
+					// blurChat();
 				}
 			}, 2000); // Check every 2 sec
 			return;
@@ -26,31 +28,88 @@ chrome.storage.sync.get("password", ({ password }) => {
 	enterPassword();
 });
 
-function modifyCSS() {
-    console.log('called modify css');
+// function blurChat() {
+// 	const interval = setInterval(() => {
+// 		const divElements = document.querySelectorAll(".CzM4m"); // Replace with your class name
+// 		if (divElements.length > 0) {
+// 			clearInterval(interval); // Clear the interval when the class is found
+// 			blurChatCSS(); // Call the function to modify the CSS
+// 			return;
+// 		} else blurChat();
+// 	}, 2000); // Check every 2 sec
+// }
+
+function blurContactCSS() {
+	console.log("called modify css contact");
 	const divAvatarElement = document.querySelectorAll("._1AHcd");
 	const divNameElement = document.querySelectorAll("._8nE1Y");
 
-	divAvatarElement.forEach((e, idx) => {
-		divAvatarElement[idx].style.filter = "blur(5px)";
-		divNameElement[idx].style.filter = "blur(5px)";
+	divAvatarElement.forEach((item, idx) => {
+		divAvatarElement[idx].classList.add("blur-css");
+		divNameElement[idx].classList.add("blur-css");
 
 		divAvatarElement[idx].addEventListener("mouseenter", () => {
-			divAvatarElement[idx].style.filter = "none";
-			divNameElement[idx].style.filter = "none";
-		});
-		divNameElement[idx].addEventListener("mouseenter", () => {
-			divNameElement[idx].style.filter = "none";
-			divAvatarElement[idx].style.filter = "none";
+			divAvatarElement[idx].classList.remove("blur-css"); // Remove blur class
+			divNameElement[idx].classList.remove("blur-css"); // Remove blur class
 		});
 
 		divAvatarElement[idx].addEventListener("mouseleave", () => {
-			divAvatarElement[idx].style.filter = "blur(5px)";
-			divNameElement[idx].style.filter = "blur(5px)";
+			divAvatarElement[idx].classList.add("blur-css"); // Add blur class
+			divNameElement[idx].classList.add("blur-css"); // Add blur class
 		});
+
+		divAvatarElement[idx].addEventListener("click", blurChatCSS);
+
+		divNameElement[idx].addEventListener("mouseenter", () => {
+			divNameElement[idx].classList.remove("blur-css"); // Remove blur class
+			divAvatarElement[idx].classList.remove("blur-css"); // Remove blur class
+		});
+
 		divNameElement[idx].addEventListener("mouseleave", () => {
-			divNameElement[idx].style.filter = "blur(5px)";
-			divAvatarElement[idx].style.filter = "blur(5px)";
+			divNameElement[idx].classList.add("blur-css"); // Add blur class
+			divAvatarElement[idx].classList.add("blur-css"); // Add blur class
 		});
+		divNameElement[idx].addEventListener("click", blurChatCSS);
 	});
+}
+
+function blurChatCSS() {
+	setInterval(() => {
+		console.log("called modify css chat");
+		const divElement = document.querySelectorAll(".CzM4m");
+		divElement.forEach((e) => {
+			e.classList.add("blur-css");
+			e.addEventListener("mouseenter", () => {
+				e.classList.remove("blur-css");
+			});
+			e.addEventListener("mouseleave", () => {
+				e.classList.add("blur-css");
+			});
+		});
+	}, 1000); // Check every 2 sec
+}
+
+function addCSS() {
+	// Step 1: Create new CSS rules
+	var newRules = [
+		".blur-css { filter:blur(3px); }",
+		".blur-none { filter:none; }",
+	];
+
+	// Step 2: Create a <style> element and append the new rules to it
+	var styleElement = document.createElement("style");
+	styleElement.type = "text/css";
+
+	if (styleElement.styleSheet) {
+		// For IE (older versions)
+		styleElement.styleSheet.cssText = newRules.join(" ");
+	} else {
+		// For other browsers
+		newRules.forEach(function (rule) {
+			styleElement.appendChild(document.createTextNode(rule));
+		});
+	}
+
+	// Step 3: Append the <style> element to the <head> of the document
+	document.head.appendChild(styleElement);
 }
